d00603334cfafbc1f6d3e849edacdba99f1ae46d
EconClass <bizmail327@gmail.com>
2018-11-08 13:23:16 -0800

    app inital

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import sys
 import string
 import random
+import cleanup
+import tokenize
+import word_count
+import sample
+import sentence
+
+# define some functions that compose the above modules
+
+if __name__ == '__main__':
+    # code to run when file is executed
