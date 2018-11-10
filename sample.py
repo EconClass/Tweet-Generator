eb437190fe2164b8c2bc75d8ad4408a1bbd9a4fc
EconClass <bizmail327@gmail.com>
2018-11-09 16:05:57 -0800

    comments

--- sample.py
+++ sample.py
@@ -3,31 +3,31 @@ import random
 from cleanup import clean_text
 from word_count import histogram, all_items
 
-def cummulative_sample(histogram_in, num_iter):
-    '''
-    This function sums the frequency of the items consecutively
-    then appends the value to the list value of the key item.
-    '''
-    max_len = all_items(histogram_in)
-    list_tuples = list(histogram_in.items())
+# def cummulative_sample(histogram_in, num_iter):
+#     '''
+#     This function sums the frequency of the items consecutively
+#     then appends the value to the list value of the key item.
+#     '''
+#     max_len = all_items(histogram_in)
+#     list_tuples = list(histogram_in.items())
 
-    iterations = 0
-    cume_sum = 0
-    index = 0
-    result = []
+#     iterations = 0
+#     cume_sum = 0
+#     index = 0
+#     result = []
 
-    while (iterations < num_iter):
-        rand_num = random.randrange( max_len )
-        while (cume_sum < rand_num):
-            index += 1
-            cume_sum += list_tuples[index][1]
-            if cume_sum >= rand_num:
-                break
+#     while (iterations < num_iter):
+#         rand_num = random.randrange( max_len )
+#         while (cume_sum < rand_num):
+#             index += 1
+#             cume_sum += list_tuples[index][1]
+#             if cume_sum >= rand_num:
+#                 break
 
-        result.append(list_tuples[index][0])
-        iterations += 1
+#         result.append(list_tuples[index][0])
+#         iterations += 1
     
-    return result
+#     return result
 
 def dictionary_sample(histogram_in, num_iter=1):
     '''
@@ -40,13 +40,22 @@ def dictionary_sample(histogram_in, num_iter=1):
     words = []
     count = 0
 
+    # Loop counts number of samples taken from dictionary
     while count < num_iter: 
         rand_num = random.randint(1, max_num)
-        for word in histogram_in:
+
+        # Iterate through all keys in dictionary
+        for word in histogram_in: 
+
+            # Add number of occurances to cummulative variable 
             cume_sum += histogram_in[word][1]
+
+            # Add word to return list and exit "for" loop
             if cume_sum >= rand_num:
                 words.append(word)
                 break
+        
+        # Adds 1 to count after for loop executes
         count +=1
     return words
 
