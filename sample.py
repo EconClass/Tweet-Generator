3840fb1cfcf524e73b6f822cb2e9a961faa6de16
EconClass <bizmail327@gmail.com>
2018-11-28 13:16:31 -0800

    added hashtable files

--- sample.py
+++ sample.py
@@ -3,32 +3,6 @@ import random
 from cleanup import clean_text
 from word_count import histogram, all_items
 
-# def cummulative_sample(histogram_in, num_iter):
-#     '''
-#     This function sums the frequency of the items consecutively
-#     then appends the value to the list value of the key item.
-#     '''
-#     max_len = all_items(histogram_in)
-#     list_tuples = list(histogram_in.items())
-
-#     iterations = 0
-#     cume_sum = 0
-#     index = 0
-#     result = []
-
-#     while (iterations < num_iter):
-#         rand_num = random.randrange( max_len )
-#         while (cume_sum < rand_num):
-#             index += 1
-#             cume_sum += list_tuples[index][1]
-#             if cume_sum >= rand_num:
-#                 break
-
-#         result.append(list_tuples[index][0])
-#         iterations += 1
-    
-#     return result
-
 def dictionary_sample(histogram_in, num_iter=1):
     '''
     This function takes in a histogram and returns 
